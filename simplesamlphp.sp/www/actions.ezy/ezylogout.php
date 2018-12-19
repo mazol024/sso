657148dc0b6e6eb5318a45821368f40c430b4422
olegmazhanov <oleg.mazhanov@gmail.com>
2018-12-19 13:42:48 +1300

    Working commit 2

--- simplesamlphp.sp/www/actions.ezy/ezylogout.php
+++ simplesamlphp.sp/www/actions.ezy/ezylogout.php
@@ -4,6 +4,4 @@ require_once('../../lib/_autoload.php');
 $as = new \SimpleSAML\Auth\Simple($_SESSION['idp']);
 
 $as->logout(array('ReturnTo' => 'https://sp01/simplesaml/ezylogin.php','ReturnStateParam' => 'LogoutState','ReturnStateStage' => 'MyLogoutState',));
-echo "<h1>Logged out!</h1>";
-
 ?>
--- simplesamlphp.sp/www/actions.ezy/mainpage.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: olegmazhanov
- * Date: 2018-12-18
- * Time: 15:27
- */
-
-echo "Main page";
